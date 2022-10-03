eb299cdf152fa6ef02383e2122f51306f757fffb
Nishant <82361039+Nishant9947@users.noreply.github.com>
2022-10-03 19:32:18 +0530

    X01AD: Enable BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES
    * this fixes compilation error below:
    
    error: found ELF prebuilt in PREBUILT_PRODUCT_COPY_FILES, use cc_prebuild_binary / cc_prebuild_library_shared instead.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,6 +44,7 @@ TARGET_BOOTLOADER_BOARD_NAME := sdm632
 
 # Build
 BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_serial_dm,0x78af000 androidboot.usbconfigfs=true loop.max_part=7
