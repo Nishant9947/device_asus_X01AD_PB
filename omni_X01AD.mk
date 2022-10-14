b18bde11eb5cf5b942aee3fe08a0b67e37a77207
Nishant <82361039+Nishant9947@users.noreply.github.com>
2022-10-14 11:25:22 +0530

    Update omni_X01AD.mk

--- omni_X01AD.mk
+++ omni_X01AD.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Charger
 PRODUCT_PACKAGES += \
